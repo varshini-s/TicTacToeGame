a0b8f3c739101b0f8391e80235e5c5fa6054a5ab
varshini <dreamvarshini@gmail.com>
2021-09-06 08:18:56 +0530

    ADDED:character array to maintain board

--- src/com/bridgelabz/tictactoe/TicTacToe.java
+++ src/com/bridgelabz/tictactoe/TicTacToe.java
@@ -8,6 +8,13 @@ public class TicTacToe
 
 		System.out.println("welcome to tic tac toe game");
 
+		char[] Board = new char[11];
+
+		for(int index=1;index<11;index++)
+		{
+			Board[index]=' ';
+		}
+
 	}
 
 }
