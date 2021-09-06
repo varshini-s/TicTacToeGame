2cb45412739a244268db2630c21aa3f046db66e1
varshini <dreamvarshini@gmail.com>
2021-09-06 08:30:11 +0530

    MODIFIED:changed board size to 10 and new method to initialise

--- src/com/bridgelabz/tictactoe/TicTacToe.java
+++ src/com/bridgelabz/tictactoe/TicTacToe.java
@@ -3,17 +3,24 @@ package com.bridgelabz.tictactoe;
 public class TicTacToe 
 {
 
+	char[] board = new char[10];
+
+	public void initializeBoard()
+	{
+		for(int index=1;index<11;index++)
+		{
+			board[index]=' ';
+		}
+
+	}
+
 	public static void main(String[] args) 
 	{
 
 		System.out.println("welcome to tic tac toe game");
 
-		char[] Board = new char[11];
 
-		for(int index=1;index<11;index++)
-		{
-			Board[index]=' ';
-		}
+
 
 	}
 
