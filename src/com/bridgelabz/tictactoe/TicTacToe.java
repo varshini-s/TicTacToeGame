048412319690d7e28bf288822fbb3b4a53262ae3
varshini <dreamvarshini@gmail.com>
2021-09-06 08:49:19 +0530

    ADDED: function to take userinput

--- src/com/bridgelabz/tictactoe/TicTacToe.java
+++ src/com/bridgelabz/tictactoe/TicTacToe.java
@@ -1,5 +1,7 @@
 package com.bridgelabz.tictactoe;
 
+import java.util.Scanner;
+
 public class TicTacToe 
 {
 
@@ -7,20 +9,34 @@ public class TicTacToe
 
 	public void initializeBoard()
 	{
-		for(int index=1;index<11;index++)
+		for(int index=1;index<board.length;index++)
 		{
 			board[index]=' ';
 		}
 
 	}
-
+	
+	public void userInput()
+	{
+		Scanner scanner = new Scanner(System.in);
+		char input =scanner.next().charAt(0);
+		input=Character.toUpperCase(input);
+		System.out.println(input);
+		
+	}
+	
 	public static void main(String[] args) 
 	{
 
 		System.out.println("welcome to tic tac toe game");
+		
+		TicTacToe game = new TicTacToe();
+		
+		game.initializeBoard();
+		game.userInput();
+		
 
-
-
+		
 
 	}
 
